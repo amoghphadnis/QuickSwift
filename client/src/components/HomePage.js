3355e537bc203014a67baf81ca45aa6d74300409
Abhinashdeep <abhinashdeep123@gmail.com>
2024-10-02 17:19:48 -0400

    Added pickup, dropoff and order summary

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { TextField, Box, Typography } from '@mui/material';
 
 const HomePage = () => {
     return (
@@ -11,9 +11,15 @@ const HomePage = () => {
             </Box>
 
             <Box sx={{ flex: 1, padding: 2 }}>
+                <TextField label="Pick-up Location" fullWidth margin="normal" />
+                <TextField label="Drop-off Location" fullWidth margin="normal" />
+
                 <Typography variant="h6" sx={{ mt: 2 }}>
                     Order Summary
                 </Typography>
+                <TextField label="Item 1" fullWidth margin="normal" />
+                <TextField label="Item 2" fullWidth margin="normal" />
+                <TextField label="Item 3" fullWidth margin="normal" />
             </Box>
         </Box>
     );
